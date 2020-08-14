5b1856710f02434dc4fbe0a8963fd9bf725b2cfb
Azmah-Bad <56384031+Azmah-Bad@users.noreply.github.com>
2020-08-14 23:52:46 +0200

    hide username key
    Signed-off-by: Azmah-Bad <56384031+Azmah-Bad@users.noreply.github.com>

--- src/app/location.service.ts
+++ src/app/location.service.ts
@@ -2,13 +2,14 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { GeoNames } from "./geo-names";
 import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class LocationService {
-  private api = "http://api.geonames.org/countryCodeJSON?"
-  private username = "_hba_"
+  private api = "http://api.geonames.org/countryCodeJSON?";
+  private username = environment.geoNameUsername;
 
   constructor(private http: HttpClient) {}
 
