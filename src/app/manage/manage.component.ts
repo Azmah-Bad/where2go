922a78f568db7e3038ce3b7973a06ba723bc5d66
Azmah-Bad <56384031+Azmah-Bad@users.noreply.github.com>
2020-08-23 10:53:56 +0200

    added instructions
    Signed-off-by: Azmah-Bad <56384031+Azmah-Bad@users.noreply.github.com>

--- src/app/manage/manage.component.ts
+++ src/app/manage/manage.component.ts
@@ -1,22 +1,40 @@
-import { Component, OnInit } from '@angular/core';
-import { ManageService } from "../services/manage.service";
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ManageService } from '../services/manage.service';
+
+import * as mapsData from 'devextreme/dist/js/vectormap-data/world.js';
+import { DxVectorMapComponent } from 'devextreme-angular';
 
 @Component({
   templateUrl: './manage.component.html',
-  styleUrls: ['./manage.component.scss']
+  styleUrls: ['./manage.component.scss'],
 })
 export class ManageComponent implements OnInit {
-  stageInstruction = ""
+  worldMap: any = mapsData.world;
+  INSTRCUTIONS = [
+    'select departure country',
+    'select open countries',
+    'select open countries with restricitions',
+    'select closed countries',
+  ];
+  stage = 0;
+  stageInstruction = this.INSTRCUTIONS[this.stage];
 
-  constructor(
-    private manger:ManageService,
-  ) { }
+  @ViewChild('theVectorMap', { static: false }) VectorMap: DxVectorMapComponent;
+
+  constructor(private manger: ManageService) {}
 
   ngOnInit(): void {
+    this.checkIsLoggedIn();
   }
 
   test() {
-    this.manger.test().subscribe()
+    this.manger.test().subscribe();
   }
 
+  /*
+  if user isn't logged in redirect him to login page
+  */
+  checkIsLoggedIn() {
+    // todo 
+  }
 }
